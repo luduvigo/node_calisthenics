51e8530f81fb7050c6b539209fb294495a7e9908
luduvigo <paoloantoniorossi@gmail.com>
2015-04-15 18:56:18 +0200

    Add headings

--- markdown2html/index.js
+++ markdown2html/index.js
@@ -1,13 +1,23 @@
-console.log("Hello world");
-
 exports.convertH1 = function (text) {
     return "<h1>" + text.substring(1) + "</h1>";
 };
 
-exports.check = function () {
-        return 1;
-};
-
 exports.convertH2 = function (text) {
-    return "<h2>" + text.substring(1) + "</h2>";
+    return "<h2>" + text.substring(2) + "</h2>";
+}
+
+exports.convertH3 = function (text) {
+    return "<h3>" + text.substring(3) + "</h3>";
+}
+
+exports.convertH4 = function (text) {
+    return "<h4>" + text.substring(4) + "</h4>";
+}
+
+exports.convertH5 = function (text) {
+    return "<h5>" + text.substring(5) + "</h5>";
+}
+
+exports.convertH6 = function (text) {
+    return "<h6>" + text.substring(6) + "</h6>";
 }
--- markdown2html/test/test.js
+++ markdown2html/test/test.js
@@ -1,11 +1,39 @@
 var index = require('../index');
 
 exports['text_h1'] = function (test) {
-    test.equal(index.convertH1("#prova"), "<h1>prova</h1>");
+    test.equal(index.convertH1("#prova"), 
+               "<h1>prova</h1>");
     test.done();
 };
 
-exports['check'] = function (test) {
-    test.equal(index.check(), 1);
+exports['text_h2'] = function (test) {
+    test.equal(index.convertH2("##prova"), 
+               "<h2>prova</h2>");
     test.done();
 };
+
+exports['text_h3'] = function (test) {
+    test.equal(index.convertH3("###prova"), 
+               "<h3>prova</h3>");
+    test.done();
+};
+
+exports['text_h4'] = function (test) {
+    test.equal(index.convertH4("####prova"), 
+               "<h4>prova</h4>");
+    test.done();
+};
+
+exports['text_h5'] = function (test) {
+    test.equal(index.convertH5("#####prova"), 
+               "<h5>prova</h5>");
+    test.done();
+};
+
+exports['text_h6'] = function (test) {
+    test.equal(index.convertH6("######prova"), 
+               "<h6>prova</h6>");
+    test.done();
+};
+
+
